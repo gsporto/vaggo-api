9f9569ac938b0296d1923ca104d596746558362c
Gabriel Porto <gabriel_porto@outlook.com>
2020-08-23 20:14:19 -0300

    user seed

---
+++ src/shared/infra/typeorm/migrations/1598134811340-SeedUsers.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner, getRepository } from 'typeorm';
+
+import CreateUserService from '@modules/users/services/CreateUserService';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import HashProvider from '@shared/container/providers/HashProvider/implementations/BCryptHashProvider';
+import CacheProvider from '@shared/container/providers/CacheProvider/implementations/RedisCacheProvider';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+
+export default class SeedUsers1598134811340 implements MigrationInterface {
+  public async up(_: QueryRunner): Promise<void> {
+    const usersRepository = new UsersRepository();
+    const hashProvider = new HashProvider();
+    const cacheProvider = new CacheProvider();
+
+    const createUser = new CreateUserService(
+      usersRepository,
+      hashProvider,
+      cacheProvider,
+    );
+    await createUser.execute({
+      name: 'Icetec',
+      email: 'comercial@icetecsolutions.com.br',
+      password: 'icetec123',
+    });
+  }
+
+  public async down(_: QueryRunner): Promise<void> {
+    const ormRepository = getRepository(User);
+
+    ormRepository
+      .createQueryBuilder()
+      .delete()
+      .where({ email: 'comercial@icetecsolutions.com.br' })
+      .execute();
+  }
+}
