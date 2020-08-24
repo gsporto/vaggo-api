aa23cdd35b0fbaebf3b3a6f5f0b5d2d947e00c95
Gabriel Porto <gabriel_porto@outlook.com>
2020-08-23 21:29:28 -0300

    container update and added routes

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -5,7 +5,15 @@ import './providers';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
+import ICandidateRepository from '@modules/candidates/repositories/ICandidateRepository';
+import CandidatesRepository from '@modules/candidates/infra/typeorm/repositories/CandidatesRepository';
+
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<ICandidateRepository>(
+  'CandidatesRepository',
+  CandidatesRepository,
 );
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 
-// import examplesRouter from '@modules/examples/infra/http/routes/examples.routes';
+import usersRouter from '@modules/users/infra/http/routes/users.routes';
+import candidatesRouter from '@modules/candidates/infra/http/routes/candidates.routes';
+import authenticateRouter from '@modules/users/infra/http/routes/Authenticate.routes';
+import profileRouter from '@modules/users/infra/http/routes/profile.routes';
 
 const routes = Router();
 
-// routes.use('/examples', examplesRouter);
+routes.use('/users', usersRouter);
+routes.use('/authenticate', authenticateRouter);
+routes.use('/me', profileRouter);
+
+routes.use('/candidates', candidatesRouter);
 
 export default routes;
